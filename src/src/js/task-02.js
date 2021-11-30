49500247a5c8112909555585ea4176b7cf53d86d
Svitlana Kaawach <svitlanakaawach@gmail.com>
2021-11-30 15:26:31 +0200

    Update task-02.js

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsListRef = document.getElementById("ingredients");
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const ingredientsList = document.createElement('li');
+  ingredientsList.classList.add('item');
+  ingredientsList.textContent = ingredient;
+
+  return ingredientsList;
+});
+console.log(ingredientsEl);
+
+// for (let i = 0; i < ingredients.length; i +=1 ) {
+//   const ingredientsList = document.createElement('li');
+//   ingredientsList.classList.add('item');
+//   ingredientsList.textContent = ingredients[i];
+//   ingredientsEl.push(ingredientsList);
+ 
+// }
+
+
+ingredientsListRef.append(...ingredientsEl);
