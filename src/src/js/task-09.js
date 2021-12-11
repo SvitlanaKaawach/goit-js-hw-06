49ae3319cbf3bcf0a7a78347a6b578a34eb29280
Svitlana Kaawach <svitlanakaawach@gmail.com>
2021-12-11 10:12:52 +0200

    Update task-09.js

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColorEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+btnChangeColorEl.addEventListener("click", onBtnChangeColor);
+
+function onBtnChangeColor() {
+  const currentColor = getRandomHexColor();
+  document.body.style.backgroundColor = currentColor;
+  spanEl.textContent = currentColor;
+}
