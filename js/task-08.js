03b9ee1f905eecd782ea8c93a602a2383b4f5e5c
Svitlana Kaawach <svitlanakaawach@gmail.com>
2021-12-13 13:12:25 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,25 @@
 const form = document.querySelector('.login-form');
-
+let alertMessage = '';
 
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit (event) {
     event.preventDefault();
+
     const formElements = event.currentTarget.elements;
+
     const mail = formElements.email.value;
     const password = formElements.password.value;
 
     const FormData = {
-        mail, password,
+        mail,
+        password,
     };
 
     
-    if (mail == "" || password == '') { return window.alert("Please, fill in all the fields!"); }
-   form.reset();
+    if (mail == '' || password == '', mail == '' && password == '')
+    { message = alert("Please, fill in all the fields!\nЗаповніть будь ласка усі поля форми!" ); }
+    form.reset();
+    console.log(FormData);
 }
+ 
