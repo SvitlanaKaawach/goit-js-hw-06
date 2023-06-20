cda8cce46c79f763a614deeca7a81701f7c43616
Svitlana Kaawach <svitlanakaawach@gmail.com>
2023-06-20 16:09:31 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,11 @@
 const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
-textInput.addEventListener('input', event => {
-	output.textContent = event.target.value
+inputEl.addEventListener('input', event => {
+	outputEl.textContent = event.target.value
 
 	if (event.target.value === '') {
-		output.textContent = 'Anonymous';
+		outputEl.textContent = 'Anonymous';
 	}
 	
 });
